f7aa635ac2c117e0f11535f308c12f395533cdd3
chris.deng <haogitcc@163.com>
2020-02-13 16:51:57 +0800

    Add new question
    Q2
    f(m, n) = n, (m=1)
    f(m, n) = m, (n=1)
    f(m, n) = f(m-1, n) + f(m, n-1), (m>1, n>1)
    1, 2, 3, 4, 5
    2, 4, 7, 11,16
    3, 7,14, 25,41
    4, 11,25,50,91
    5, 16,41,91,182
    如上所示，假如想求m为3、n为4的f(m, n)值，就是25。

--- cpp/PRO_CPP/PRO_CPP.cpp
+++ cpp/PRO_CPP/PRO_CPP.cpp
@@ -1,11 +1,6 @@
 ﻿// PRO_CPP.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
-/*
-输入连个字符串
-输出第二个字符串在第一个字符串中的连接次序
-例如，输入abdbcc和abc，输出125,126,145,146
-*/
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
@@ -14,16 +9,98 @@ using namespace std;
 void ConnectSequence(char* p_str, char* s_str);
 void PrintfArray(char* p_str, char* s_str, int* print_arr, int p_len, int s_len, int print_arr_num, int p_start_num, int s_start_num);
 
+int func(int m, int n);
+
 int main()
 {
+    /*
+    //Q1
+    输入连个字符串
+    输出第二个字符串在第一个字符串中的连接次序
+    例如，输入abdbcc和abc，输出125,126,145,146
+    */
     char ParString[] = "abdbcca";
     char SonString[] = "abc";
+    
+    //ConnectSequence(ParString, SonString);
 
-    ConnectSequence(ParString, SonString);
+    /*
+    //Q2
+    f(m, n) = n, (m=1)
+    f(m, n) = m, (n=1)
+    f(m, n) = f(m-1, n) + f(m, n-1), (m>1, n>1)
+    1, 2, 3, 4, 5
+    2, 4, 7, 11,16
+    3, 7,14, 25,41
+    4, 11,25,50,91
+    5, 16,41,91,182
+    如上所示，假如想求m为3、n为4的f(m, n)值，就是25。
+    */
+    cout << "Q2: " << func(3, 4) << endl;
 
     return 0;
 }
 
+//start Q2
+#define RECUSION 0
+#define NO_RECUSION 1
+
+#if RECUSION
+int func(int m, int n)
+{
+    if (m <= 0 || n <= 0)
+    {
+        return -1;
+    }
+    if (m == 1)
+    {
+        return n;
+    }
+    if (n == 1)
+    {
+        return m;
+    }
+    return func(m - 1, n) + func(m, n - 1);
+}
+#endif
+
+#if NO_RECUSION
+int func(int m, int n)
+{
+    if (m > 0 && n > 0)
+    {
+        if (m > 100 || n > 100)
+        {
+            return -2;
+        }
+        int a[100][100];
+        for (int i = 0; i != m; ++i)
+        {
+            a[i][0] = i + 1;
+        }
+        for (int i = 0; i != n; ++i)
+        {
+            a[0][i] = i + 1;
+        }
+
+        for (int i = 1; i != m; ++i)
+        {
+            for (int j = 1; j != n; ++j)
+            {
+                a[i][j] = a[i - 1][j] + a[i][j - 1];
+            }
+        }
+        return a[m - 1][n - 1];
+    }
+    else
+    {
+        return -1;
+    }
+}
+#endif
+//Q2 end
+
+// start Q1
 void ConnectSequence(char* p_str, char* s_str)
 {
     int p_len = strlen(p_str);
@@ -77,3 +154,4 @@ void PrintfArray(char *p_str, char *s_str, int *print_arr, int p_len, int s_len,
         }
     }
 }
+// Q1 end
